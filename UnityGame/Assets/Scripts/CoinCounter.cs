e7afe119826d5edc4f7ad97e777376e42a3219d9
Frank Flamme <github@lsks-consulting.de>
2021-07-03 22:50:13 +0200

    Add coin counter.

---
+++ UnityGame/Assets/Scripts/CoinCounter.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CoinCounter : MonoBehaviour
+{
+    private LevelManager _levelManager;
+
+    [Header("Digit Coin Sprites")]
+    [SerializeField]
+    private Sprite[] _digitSprites;
+
+    [SerializeField]
+    private Sprite _coinSprite;
+
+    [Header("Coin Digits")]
+    [SerializeField]
+    private Image _ones;
+
+    [SerializeField]
+    private Image _tenth;
+
+    [SerializeField]
+    private Image _hundreds;
+
+    [SerializeField]
+    private Image _coin;
+
+    public int maxPoints;
+    float points = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _levelManager = FindObjectOfType<LevelManager>();
+        _coin.sprite = this._coinSprite;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        points = _levelManager.coinCount;
+
+        if (points <= 0)
+        {
+            _ones.sprite = this._digitSprites[0];
+            _tenth.sprite = this._digitSprites[0];
+            _hundreds.sprite = this._digitSprites[0];
+            return;
+        }
+
+        int onesNumber = Mathf.RoundToInt(points % 10);
+        int tenthNumber = Mathf.RoundToInt((points - onesNumber) % 100) / 10;
+        int hundredsNumber = Mathf.RoundToInt((points - tenthNumber * 10 - onesNumber) % 1000 / 100);
+
+        _ones.sprite = this._digitSprites[onesNumber];
+        _tenth.sprite = this._digitSprites[tenthNumber];
+        _hundreds.sprite = this._digitSprites[hundredsNumber];
+    }
+}
